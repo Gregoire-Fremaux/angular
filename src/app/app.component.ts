36aa833c0cc2aba66fa12b1f1fb7f0ba65b15bf0
Grégoire Frémaux <contact@gregoire-fremaux.fr>
2020-02-06 09:21:47 +0100

    testunit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,41 @@ export class AppComponent {
   title = 'jenk-app';
   email = new FormControl('', [Validators.required, Validators.email]);
 
+  hideForm: Boolean = false;
+
+
+  mailValue;
+  passwordValue;
+
   getErrorMessage() {
     return this.email.hasError('required') ? 'You must enter a value' :
         this.email.hasError('email') ? 'Not a valid email' :
             '';
   }
+
+  submit(){
+    if(this.email.hasError('required')){
+
+    }else{
+      this.hideForm = true;
+      this.success();
+    }
+  }
+
+  success(){
+    return "OK";
+  }
+
+
+  onKey(event: any) { // without type info
+    this.mailValue = event.target.value;
+    console.log(this.mailValue)
+  }
+
+  onKey2(event: any) { // without type info
+    this.passwordValue = event.target.value;
+    console.log(this.passwordValue)
+  }
+
+
 }
---
+++ src/app/testunit.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+
+import { TestunitService } from './testunit.service';
+
+describe('TestunitService', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('should be created', () => {
+    let toto = "tamereenslip"
+    let michel = "tamereenslipE"
+
+    expect(michel).toBe("tamereenslip");
+    
+  });
+});
